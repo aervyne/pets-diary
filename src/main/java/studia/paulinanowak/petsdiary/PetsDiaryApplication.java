b8ea9c9ee3729ba48e48df00941dbb05bb3e7bd9
paulina.nowak <paulina.nowak@librus.pl>
2022-10-20 13:41:49 +0200

    Initial project.

---
+++ src/main/java/studia/paulinanowak/petsdiary/PetsDiaryApplication.java
@@ -0,0 +1,13 @@
+package studia.paulinanowak.petsdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/studia/paulinanowak/petsdiary/PetsDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package studia.paulinanowak.petsdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
