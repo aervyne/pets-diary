6604cac6607e3f8ca579885baf4a353be927c9b4
paulina.nowak <paulina.nowak@librus.pl>
2022-10-24 12:04:20 +0200

    Dodanie podstawowego POJO

---
+++ src/main/java/studia/paulinanowak/petsdiary/model/Pet.java
@@ -0,0 +1,45 @@
+package studia.paulinanowak.petsdiary.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private String name;
+    private LocalDate dateOfBirth;
+    private PetType petType;
+    private String breed;
+
+    // TODO dodać pozostałe pola zgodnie z diagramem
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
---
+++ src/main/java/studia/paulinanowak/petsdiary/model/PetType.java
@@ -0,0 +1,13 @@
+package studia.paulinanowak.petsdiary.model;
+
+public class PetType {
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
