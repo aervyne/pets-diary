a5e75e06bb6553f472cfb63c74e56286f0ac88a3
paulina.nowak <paulina.nowak@librus.pl>
2022-10-24 13:10:14 +0200

    Dodanie getterów i setterów dla base entity

--- src/main/java/studia/paulinanowak/petsdiary/model/BaseEntity.java
+++ src/main/java/studia/paulinanowak/petsdiary/model/BaseEntity.java
@@ -4,4 +4,12 @@ import java.io.Serializable;
 
 public class BaseEntity implements Serializable {
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
