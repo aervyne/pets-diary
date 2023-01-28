9e04818d03d5fe60ac129195cc57b8c0f01c384e
Paulinka <nowakpaulina319@gmail.com>
2023-01-28 20:45:02 +0100

    Dodanie szczegółów zwierzaka i zmiany rozmiaru zdjęcia

--- src/main/java/studia/paulinanowak/petsdiary/services/springdatajpa/ImageSDJpaService.java
+++ src/main/java/studia/paulinanowak/petsdiary/services/springdatajpa/ImageSDJpaService.java
@@ -1,5 +1,6 @@
 package studia.paulinanowak.petsdiary.services.springdatajpa;
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +9,10 @@ import studia.paulinanowak.petsdiary.model.Pet;
 import studia.paulinanowak.petsdiary.repositories.PetRepository;
 import studia.paulinanowak.petsdiary.services.ImageService;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 @Service
@@ -22,15 +27,38 @@ public class ImageSDJpaService implements ImageService {
     @Override
     @Transactional
     public void saveImageFile(Long petId, MultipartFile file) {
-
         try {
             Pet pet = petRepository.findById(petId).get();
 
-            Byte[] byteObjects = new Byte[file.getBytes().length];
+            // reads input image
+            BufferedImage inputImage = ImageIO.read(file.getInputStream());
+
+            BufferedImage bufferedImageResult = new BufferedImage(
+                    350,
+                    350,
+                    inputImage.getType()
+            );
+
+            Graphics2D g2d = bufferedImageResult.createGraphics();
+            g2d.drawImage(
+                    inputImage,
+                    0,
+                    0,
+                    350,
+                    350,
+                    null
+            );
+            g2d.dispose();
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImageResult, FilenameUtils.getExtension(file.getOriginalFilename()), baos);
+            byte[] bytes = baos.toByteArray();
+
+            Byte[] byteObjects = new Byte[baos.toByteArray().length];
 
             int i = 0;
 
-            for (byte b : file.getBytes()){
+            for (byte b : baos.toByteArray()){
                 byteObjects[i++] = b;
             }
 
